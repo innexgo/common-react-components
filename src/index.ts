95dc4c8acfd0fd1303ff78c5a2b6da59b3d91396
gpi <gpimpale29@gmail.com>
2021-09-01 23:05:18 -0700

    add Action, for an icon + text that may be clicked on

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import Branding from './components/Branding';
 import BrandedComponentProps from './components/BrandedComponentProps';
 import Loader from './components/Loader';
+import Action from './components/Action';
 import Section from './components/Section';
 import SimpleLayout from './components/SimpleLayout';
 
@@ -8,5 +9,6 @@ export type { BrandedComponentProps };
 export type { Branding };
 export { Section };
 export { Loader };
+export { Action };
 export { SimpleLayout };
 
